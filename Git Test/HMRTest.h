30383cbef37589f700912deeda1c230aa475b13f
Hafiz Mohd Rozlan <hafizmohdrozlan@HAFIZs-MacBook-Air.local>
2014-05-08 11:37:28 +0800

    added a test class

---
+++ Git Test/HMRTest.h
@@ -0,0 +1,13 @@
+//
+//  HMRTest.h
+//  Git Test
+//
+//  Created by Hafiz Mohd Rozlan on 8/5/14.
+//  Copyright (c) 2014 Hafiz M. Rozlan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMRTest : NSObject
+
+@end
---
+++ Git Test/HMRTest.m
@@ -0,0 +1,13 @@
+//
+//  HMRTest.m
+//  Git Test
+//
+//  Created by Hafiz Mohd Rozlan on 8/5/14.
+//  Copyright (c) 2014 Hafiz M. Rozlan. All rights reserved.
+//
+
+#import "HMRTest.h"
+
+@implementation HMRTest
+
+@end
